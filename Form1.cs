a688d58ae57547c3f148f81df444671845df350c
AfroMokus <kirschjanosmark@gmail.com>
2023-11-24 16:02:15 +0100

    működik az összes példával

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LezerVagoHazszam
 {
@@ -15,6 +16,23 @@ namespace LezerVagoHazszam
         public Form1()
         {
             InitializeComponent();
+
+            /*string fileNev = "Márknak.txt";
+
+            StreamReader str = new StreamReader(fileNev);
+
+            foreach (string megrendeles in str.ReadToEnd().Split(new string[] { "Eladás" }, StringSplitOptions.None)) 
+            {
+                if (megrendeles != "")
+                {                    
+                    richTextBoxInput.Text += feldolgozMegrendelest(megrendeles).atirCSVFormat() + "\n";
+                }
+            }
+
+            //MessageBox.Show(i + "");
+
+            str.Close();*/
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,84 +49,152 @@ namespace LezerVagoHazszam
         {
             richTextBoxInput.Text = "";
 
-            string szoveg = "";
+            richTextBoxInput.Paste();
+
+            richTextBoxOutput.Text = feldolgozMegrendelest(richTextBoxInput.Text).atirCSVFormat();
+
+            System.Windows.Forms.Clipboard.SetText(richTextBoxOutput.Text);
+        }
+
+        private Aru feldolgozMegrendelest(string megrendelesSzoveg)
+        {/*try
+            {*/
 
             Aru aru = new Aru();
 
-            richTextBoxInput.Paste();
+            string szoveg = "";
 
-            /*try
-            {*/
-                szoveg = richTextBoxInput.Text.Split(new string[] { " vásárlónak\n" }, StringSplitOptions.None)[1];
-            szoveg = szoveg.Split('\n')[0];
+            szoveg = megrendelesSzoveg.Split(new string[] { " vásárlónak" }, StringSplitOptions.None)[1];
+            szoveg = szoveg.Split('\n')[1];
 
-            if (!szoveg.Contains("-én"))
+            if (!szoveg.Contains("-én") && !szoveg.Contains("-án"))
             {
                 aru.idopont = DateTime.Parse(szoveg);
             }
             else
             {
-                szoveg = szoveg.Remove(szoveg.Length - 3, 3);
-                MessageBox.Show(szoveg);
+                szoveg = szoveg.Remove(szoveg.Length - 4, 3);
                 aru.idopont = DateTime.Parse(szoveg);
-                MessageBox.Show(aru.idopont + "");
             }
 
-                szoveg = richTextBoxInput.Text.Split(new string[] { "Termék ára\n" }, StringSplitOptions.None)[1];
-            szoveg = szoveg = szoveg.Split('\n')[0];
-            szoveg = szoveg.Remove(szoveg.Length - 2, 2);
-                szoveg = string.Concat(szoveg.Where(c => !Char.IsWhiteSpace(c)));
+            try
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "Termék ára" }, StringSplitOptions.None)[1];
+                szoveg = szoveg = szoveg.Split('\n')[1];
+            }
+            catch 
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "RENDELÉSRE" }, StringSplitOptions.None)[1];
+                szoveg = szoveg = szoveg.Split('\n')[2];
+            }
+            szoveg = szoveg.Remove(szoveg.Length - 3, 2);
+            szoveg = string.Concat(szoveg.Where(c => !Char.IsWhiteSpace(c)));
             aru.ar = Convert.ToInt32(szoveg);
 
-                szoveg = richTextBoxInput.Text.Split(new string[] { "logo" }, StringSplitOptions.None)[0];
-                szoveg = szoveg.Split('\n').Last<string>();
-                aru.szallitasiMod = szoveg;
+            szoveg = megrendelesSzoveg.Split(new string[] { "logo" }, StringSplitOptions.None)[0];
+            szoveg = szoveg.Split('\n').Last<string>();
+            aru.szallitasiMod = szoveg;
 
-                szoveg = richTextBoxInput.Text.Split(new string[] { "Szállítás\n\n" }, StringSplitOptions.None)[1];
-                szoveg = szoveg.Split(new string[] { "Ft" }, StringSplitOptions.None)[0];
-                szoveg = szoveg.Trim();
-                szoveg = string.Concat(szoveg.Where(c => !Char.IsWhiteSpace(c)));
-                aru.szamlazottPostaKolts = Convert.ToInt32(szoveg);
+            aru.szallitasiMod = aru.szallitasiMod.Trim();
 
-                szoveg = richTextBoxInput.Text.Split(new string[] { "Számlaszám:\n" }, StringSplitOptions.None)[1];
+            try
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "Termék ára" }, StringSplitOptions.None)[1];
+                szoveg = szoveg.Split(new string[] { "Szállítás" }, StringSplitOptions.None)[1];
+            }
+            catch
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "RENDELÉSRE" }, StringSplitOptions.None)[1];
+                szoveg = szoveg.Split(new string[] { "Szállítás" }, StringSplitOptions.None)[3];
+            }
+
+            szoveg = szoveg.Split(new string[] { "Ft" }, StringSplitOptions.None)[0];
+            //szoveg = szoveg.Trim();
+            szoveg = string.Concat(szoveg.Where(c => !Char.IsWhiteSpace(c)));
+            aru.szamlazottPostaKolts = Convert.ToInt32(szoveg);
+
+            if (megrendelesSzoveg.Split(new string[] { "Számlaszám:" }, StringSplitOptions.None).Count() > 1)
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "Számlaszám:" }, StringSplitOptions.None)[1];
                 szoveg = szoveg.Split('\n')[0];
                 aru.szamlaSzam = szoveg;
+            }
+            else 
+            {
+                aru.szamlaSzam = "";
+            }
 
-                szoveg = richTextBoxInput.Text.Split(new string[] { "Megrendelő\n" }, StringSplitOptions.None)[1];
-                szoveg = szoveg.Split('\n')[1];
-                aru.vasarloNeve = szoveg;
+            aru.szamlaSzam = aru.szamlaSzam.Trim();
 
-                if (richTextBoxInput.Text.Contains("Fizetési státusz\nFizetve"))
-                {
-                    aru.fizetesiMod = "barion";
-                }
-                else
-                {
-                    aru.fizetesiMod = "nincs fizetve";
-                }
+            szoveg = megrendelesSzoveg.Split(new string[] { "Megrendelő" }, StringSplitOptions.None)[1];
+            szoveg = szoveg.Split('\n')[2];
+            aru.vasarloNeve = szoveg;
 
-                if (richTextBoxInput.Text.Contains("utcanévtábla"))
-                {
-                    szoveg = richTextBoxInput.Text.Split(new string[] { "- méret: " }, StringSplitOptions.None)[1];
-                    szoveg = szoveg.Split('\n')[0];
-                    szoveg = szoveg.Remove(0, 3);
-                    //méret
-                    aru.termeknev = "házszám, " + szoveg;
-                }
-                else
-                {
-                    MessageBox.Show("Ismeretlen áru!");
-                }
+            aru.vasarloNeve = aru.vasarloNeve.Trim();
 
-                richTextBoxOutput.Text = aru.atirCSVFormat();
+            if (megrendelesSzoveg.Contains("Fizetési státusz\nFizetve"))
+            {
+                aru.fizetesiMod = "barion";
+            }
+            else
+            {
+                aru.fizetesiMod = "nincs fizetve";
+            }
 
-                System.Windows.Forms.Clipboard.SetText(richTextBoxOutput.Text);
+            aru.fizetesiMod = aru.fizetesiMod.Trim();
+
+            if (megrendelesSzoveg.Contains("utcanévtábla") || megrendelesSzoveg.Contains("házszámtábla"))
+            {
+                szoveg = megrendelesSzoveg.Split(new string[] { "- méret: " }, StringSplitOptions.None)[1];
+                szoveg = szoveg.Split('\n')[0];
+                szoveg = szoveg.Remove(0, 3);
+                //méret
+                aru.termeknev = "Házszám, " + szoveg;
+            }
+            else if (megrendelesSzoveg.Contains("5 forintos emlékérme tartó"))
+            {
+                aru.termeknev = "5 forintos emlékérme tartó";
+            }
+            else if (megrendelesSzoveg.Contains("Gyerek bejelentő ajándék"))
+            {
+                aru.termeknev = "Gyerek bejelentő ajándék";
+            }
+            else if (megrendelesSzoveg.Contains("Római számos hangtalan falióra, Hangtalan óraszerkezettel"))
+            {
+
+                szoveg = megrendelesSzoveg.Split(new string[] { "- méret: " }, StringSplitOptions.None)[1];
+                szoveg = szoveg.Split('\n')[0];
+                szoveg = szoveg.Remove(0, 4);
+                //méret
+                aru.termeknev = "Római számos falióra, " + szoveg;
+
+            }
+            else if (megrendelesSzoveg.Contains("Különleges dominó óra, Gyerekszobába hangtalan játékos falióra"))
+            {
+
+                szoveg = megrendelesSzoveg.Split(new string[] { "- méret: " }, StringSplitOptions.None)[1];
+                szoveg = szoveg.Split('\n')[0];
+                szoveg = szoveg.Remove(0, 1);
+                szoveg = szoveg.Remove(szoveg.Length - 8, 7);
+                //méret
+                aru.termeknev = "Dominó óra, " + szoveg;
+
+            }
+            else
+            {
+                aru.termeknev = "Ismeretlen áru!";
+                MessageBox.Show("Ismeretlen áru!");
+            }
+
+            aru.termeknev = aru.termeknev.Trim();
             /*}
             catch 
             {
                 MessageBox.Show("Hibás beviteli adat!\n\n");
             }*/
 
+
+            return aru;
         }
 
         private void button1_Click(object sender, EventArgs e)
